6f5da58ccbd0c0b86e3c53bd45ec1f4599e08e01
nis224 <nis224@lehigh.edu>
2024-04-30 22:48:11 -0400

    update

--- dashboard.py
+++ dashboard.py
@@ -16,16 +16,30 @@ def main():
         st.title('Predicting Fraud in Financial Transactions')
         img = Image.open('pics/photo.jpeg')
         st.image(img)
+
+        st.header('Data Overview')
+        st.write(data.head(10))
         
-        st.text('Fixed width text')
-        st.markdown('_Markdown_') # see #*
-        st.caption('Balloons. Hundreds of them...')
-        st.latex(r''' e^{i\pi} + 1 = 0 ''')
-        st.write('Most objects') # df, err, func, keras!
-        st.write(['st', 'is <', 3]) # see *
-        st.title('My title')
-        st.header('My header')
-        st.subheader('My sub')
+        st.header('Correlation Matrix')
+        #st.subheader('My sub')
+        img = Image.open('pics/plot1.png')
+        st.image(img)
+
+        st.header('Confusion Matrix')
+        #st.subheader('My sub')
+        img = Image.open('pics/plot2.png')
+        st.image(img)
+
+        st.header('Precision Recall Curve')
+        #st.subheader('My sub')
+        img = Image.open('pics/plot3.png')
+        st.image(img)
+
+        st.header('ROC Curve')
+        #st.subheader('My sub')
+        img = Image.open('pics/plot4.png')
+        st.image(img)
+        
         st.code('for i in range(8): foo()')
 
     if page == "Report":
